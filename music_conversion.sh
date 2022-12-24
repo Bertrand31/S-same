b9aa7de767a5e02a5754f9d7a3ca00fcaab73905
Bertrand31 <bertrandjun@gmail.com>
2022-12-24 12:01:18 +0100

    Move script

--- music_conversion.sh
+++ music_conversion.sh
@@ -1,6 +1,6 @@
 function convertWav() {
     FILENAME=$(echo "$1" | cut -d "/" -f 3- | cut -d "." -f -2).wav
-    ffmpeg -y -i "$1" -ac 1 -c:a pcm_s16le -ar 44100 "data/$FILENAME" 2> /dev/null
+    ffmpeg -y -i "$1" -ac 1 -c:a pcm_s16le -ar 44100 "data/processedFiles/$FILENAME"
     echo "=> Converted '$1' to '$FILENAME'"
 }
 
