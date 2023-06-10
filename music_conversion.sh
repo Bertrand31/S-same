ab19e79606a967dfe76750b5225062869d90da95
Bertrand31 <bertrandjun@gmail.com>
2023-06-10 16:46:51 +0200

    Improve conversion script logging

--- music_conversion.sh
+++ music_conversion.sh
@@ -1,7 +1,7 @@
 function convertWav() {
     FILENAME=$(echo "$1" | cut -d "/" -f 3- | cut -d "." -f -2).wav
-    ffmpeg -y -i "$1" -ac 1 -c:a pcm_s16le -ar 44100 "data/processedFiles/$FILENAME"
-    echo "=> Converted '$1' to '$FILENAME'"
+    ffmpeg -y -i "$1" -ac 1 -c:a pcm_s16le -ar 44100 "data/processedFiles/$FILENAME" \
+    && echo "=> Converted '$1' to '$FILENAME'"
 }
 
 export -f convertWav
